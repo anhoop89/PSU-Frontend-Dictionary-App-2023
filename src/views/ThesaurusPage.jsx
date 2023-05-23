2aa7957e812155b26cad7ba93ba1c3a3d68b7661
lee-112 <leeian@pdx.edu>
2023-05-23 11:43:28 -0700

    Correct filename

--- src/views/ThesaurusPage.jsx
+++ src/views/ThesaurusPage.jsx
@@ -32,7 +32,7 @@ const ThesaurusPage = () => {
             style={{ backgroundColor: "var(--bs-dark)" }}
           >
             <h2 className="text-capitalize mb-3" style={{ fontSize: "3rem" }}>
-              {data.meta.id} <i style={{ fontSize: "1rem" }}>"{data.fl}"</i>
+              {data.meta.id} <i style={{ fontSize: "1rem" }}>"{data.fl}".</i>
             </h2>
             <b className="mt-4">Definition:</b>
             <p className="cap-first">{data.shortdef[0]}</p>
