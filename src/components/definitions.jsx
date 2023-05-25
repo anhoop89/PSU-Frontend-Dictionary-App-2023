9f1443670e32ad0c1071a3343d836ca109992ba3
miles <miles4599@gmail.com>
2023-05-25 15:59:29 -0700

    Fixed small visual bug

--- src/components/definitions.jsx
+++ src/components/definitions.jsx
@@ -17,6 +17,7 @@ export default function Definitions({ data }) {
           style={{
             backgroundColor: 'var(--bs-dark)',
           }}
+          className="rounded"
         >
           <div
             className={`${
