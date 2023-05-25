f76bcb4afb56e9427f30a987886e008fb722924a
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2023-05-25 13:38:12 -0700

    using apikey from .env again. it worked

--- src/api/WebsterAPI.jsx
+++ src/api/WebsterAPI.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
-// for some reason, it doesn't recognize the key through .env file
-//const apiKey = process.env.REACT_APP_Spanish_Key;
-const apiKey = "877a0bd4-cb72-4741-8265-41e318755512";
+// for some reason, it doesn't recognize the key through .env file sometimes
+const apiKey = process.env.REACT_APP_Spanish_Key;
+// testing this key worked
+// const apiKey = "877a0bd4-cb72-4741-8265-41e318755512";
 const url = "https://www.dictionaryapi.com/api/v3/references/spanish/json/";
 
 const WebsterAPI = async (word) => {
