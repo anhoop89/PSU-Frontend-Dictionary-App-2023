a3e48c8e2e42ec027dd5603861072c69e0912773
miles <miles4599@gmail.com>
2023-05-25 15:47:35 -0700

    implemented mouse hover effect on definitions

--- src/components/definitions.jsx
+++ src/components/definitions.jsx
@@ -1,14 +1,39 @@
+import { useState } from 'react';
+
 export default function Definitions({ data }) {
+  const [isHovering, setIsHovering] = useState(-1);
+
   return (
     <div>
-      <h1 className="card-header text-capitalize p-3">{data.word}</h1>
+      <h1
+        className="card-header rounded text-center text-capitalize my-4"
+        style={{ backgroundColor: 'var(--bs-dark)' }}
+      >
+        {data.word}
+      </h1>
       {data.definitions.map((definition, index) => (
         <div
           key={index}
-          className="rounded my-1"
-          style={{ backgroundColor: 'var(--bs-dark)' }}
+          style={{
+            backgroundColor: 'var(--bs-dark)',
+          }}
         >
-          <div className="card-body p-3">
+          <div
+            className={`${
+              isHovering === index
+                ? 'card-body p-3 rounded'
+                : 'card-body p-3 rounded border border-dark'
+            }`}
+            onMouseEnter={() => setIsHovering(index)}
+            onMouseLeave={() => setIsHovering(-1)}
+            style={
+              isHovering === index
+                ? {
+                    backgroundColor: '#191919',
+                  }
+                : { backgroundColor: 'var(--bs-dark)' }
+            }
+          >
             <p className="font-weight-bold font">
               <span className="mr-2">{index + 1}.</span>
               <i>{definition.partOfSpeech.toString()}: </i>
