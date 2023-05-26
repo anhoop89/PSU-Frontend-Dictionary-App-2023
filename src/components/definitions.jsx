a67d59bb651bfb55e059cf72aaa0e9933a1d477e
miles <miles4599@gmail.com>
2023-05-26 11:54:33 -0700

    working collapse/expand button for definition list

--- src/components/definitions.jsx
+++ src/components/definitions.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 
 export default function Definitions({ data }) {
+  const collapsed = 6; // Max number of definitions to display when 'collapsed'
+  const expanded = 100; // Max number of definitions to display when 'expanded'
+
   const [isHovering, setIsHovering] = useState(-1);
+  const [maxIndex, setMaxIndex] = useState(collapsed);
+
   const defs = data.definitions;
 
   // Sort the definitions by their 'part of speech'
@@ -28,43 +33,66 @@ export default function Definitions({ data }) {
       >
         {data.word}
       </h1>
-      {defs.map((def, index) => (
-        <>
-          {def.isTitle && (
-            <h3 className="mt-3 pl-3">
-              <i>{def.partOfSpeech}</i>
-            </h3>
-          )}
-          <div key={index} className="rounded">
-            <div
-              onMouseEnter={() => setIsHovering(index)}
-              onMouseLeave={() => setIsHovering(-1)}
-              className={`${
-                isHovering === index
-                  ? 'card-body p-3 rounded'
-                  : 'card-body p-3 rounded border border-dark'
-              }`}
-              style={
-                isHovering === index
-                  ? { backgroundColor: 'var(--bs-darkest)' }
-                  : { backgroundColor: 'var(--bs-dark)' }
-              }
-            >
-              <div className="d-flex font-weight-bold font">
+      {defs.map(
+        (def, index) =>
+          index < maxIndex && (
+            <>
+              {def.isTitle && (
+                <h3 className="mt-3 pl-3">
+                  <i>{def.partOfSpeech}</i>
+                </h3>
+              )}
+              <div key={index} className="rounded">
                 <div
-                  className="mr-3"
-                  style={{ fontSize: '1.20rem', minWidth: '30px' }}
+                  onMouseEnter={() => setIsHovering(index)}
+                  onMouseLeave={() => setIsHovering(-1)}
+                  className={`${
+                    isHovering === index
+                      ? 'card-body p-3 rounded'
+                      : 'card-body p-3 rounded border border-dark'
+                  }`}
+                  style={
+                    isHovering === index
+                      ? { backgroundColor: 'var(--bs-darkest)' }
+                      : { backgroundColor: 'var(--bs-dark)' }
+                  }
                 >
-                  {index + 1}.
-                </div>
-                <div className="cap-first definition-text" id="definition-text">
-                  {def.definition.toString()}.
+                  <div className="d-flex font-weight-bold font">
+                    <div
+                      className="mr-3"
+                      style={{ fontSize: '1.20rem', minWidth: '30px' }}
+                    >
+                      {index + 1}.
+                    </div>
+                    <div
+                      className="cap-first definition-text"
+                      id="definition-text"
+                    >
+                      {def.definition.toString()}.
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </>
-      ))}
+            </>
+          ),
+      )}
+      {defs.length > collapsed ? (
+        <div className="d-flex justify-content-center">
+          <button
+            type="button"
+            className="btn btn-secondary m-4"
+            onClick={() => {
+              maxIndex === collapsed
+                ? setMaxIndex(expanded)
+                : setMaxIndex(collapsed);
+            }}
+          >
+            {`${maxIndex === collapsed ? 'Show All' : 'Collapse'}`}
+          </button>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
