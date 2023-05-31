9402562ce902bc4b61f2fa24b7d2647646937452
miles <miles4599@gmail.com>
2023-05-30 17:12:22 -0700

    working definitions bar graph

--- src/components/definitions.jsx
+++ src/components/definitions.jsx
@@ -1,40 +1,138 @@
+// import { useState } from 'react';
+
+// export default function Definitions({ data }) {
+//   const [isHovering, setIsHovering] = useState(-1);
+
+//   return (
+//     <div>
+//       <h1
+//         className="card-header rounded text-center text-capitalize my-4"
+//         style={{ backgroundColor: 'var(--bs-dark)' }}
+//       >
+//         {data.word}
+//       </h1>
+//       {data.results.map((definition, index) => (
+//         <div key={index} className="rounded">
+//           <div
+//             onMouseEnter={() => setIsHovering(index)}
+//             onMouseLeave={() => setIsHovering(-1)}
+//             className={`${
+//               isHovering === index
+//                 ? 'card-body p-3 rounded'
+//                 : 'card-body p-3 rounded border border-dark'
+//             }`}
+//             style={
+//               isHovering === index
+//                 ? { backgroundColor: '#191919' }
+//                 : { backgroundColor: 'var(--bs-dark)' }
+//             }
+//           >
+//             <p className="font-weight-bold font">
+//               <span className="mr-2">{index + 1}.</span>
+//               <i>{definition.partOfSpeech.toString()}: </i>
+//             </p>
+//             <p className="cap-first">{definition.definition.toString()}.</p>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
 import { useState } from 'react';
 
 export default function Definitions({ data }) {
-  const [isHovering, setIsHovering] = useState(-1);
+  const min = 5; // Number of definitions to display when 'collapsed'
+  const max = 100; // Number of definitions to display when 'expanded'
+
+  const [hovering, setHovering] = useState(-1);
+  const [defsVisible, setDefsVisible] = useState(min);
+
+  const defs = data.results;
+
+  // Sort the definitions by their 'part of speech'
+  defs.sort((a, b) => a.partOfSpeech > b.partOfSpeech);
+
+  // Assign a 'title' flag to the first of each type of definition. This flag will
+  // be used to determine where to display the partsOfSpeech section titles of the
+  // definition list output.
+  for (let i = 0; i < defs.length; i += 1) {
+    if (i === 0) {
+      defs[i].isTitle = true;
+    } else if (defs[i].partOfSpeech === defs[i - 1].partOfSpeech) {
+      defs[i].isTitle = false;
+    } else {
+      defs[i].isTitle = true;
+    }
+  }
 
   return (
-    <div>
+    <div className="p-4">
       <h1
-        className="card-header rounded text-center text-capitalize my-4"
-        style={{ backgroundColor: 'var(--bs-dark)' }}
+        className="card-header rounded text-capitalize mt-2"
+        style={{ backgroundColor: 'var(--bs-darker)', fontSize: '3rem' }}
       >
         {data.word}
       </h1>
-      {data.definitions.map((definition, index) => (
-        <div key={index} className="rounded">
-          <div
-            onMouseEnter={() => setIsHovering(index)}
-            onMouseLeave={() => setIsHovering(-1)}
-            className={`${
-              isHovering === index
-                ? 'card-body p-3 rounded'
-                : 'card-body p-3 rounded border border-dark'
-            }`}
-            style={
-              isHovering === index
-                ? { backgroundColor: '#191919' }
-                : { backgroundColor: 'var(--bs-dark)' }
-            }
-          >
-            <p className="font-weight-bold font">
-              <span className="mr-2">{index + 1}.</span>
-              <i>{definition.partOfSpeech.toString()}: </i>
-            </p>
-            <p className="cap-first">{definition.definition.toString()}.</p>
+      <div className="rounded border border-secondary">
+        {defs.map(
+          (def, index) =>
+            index < defsVisible && (
+              <div key={index} className="rounded">
+                {def.isTitle && (
+                  <div
+                    className="d-flex rounded "
+                    style={{ backgroundColor: 'var(--bs-dark)' }}
+                  >
+                    <h4 className="mt-2 pl-3">
+                      <i>{def.partOfSpeech}</i>
+                    </h4>
+                  </div>
+                )}
+                <div
+                  onMouseEnter={() => setHovering(index)}
+                  onMouseLeave={() => setHovering(-1)}
+                  className="card-body p-3 rounded border border-dark"
+                  style={
+                    hovering === index
+                      ? { backgroundColor: 'var(--bs-dark)' }
+                      : { backgroundColor: 'var(--bs-darker)' }
+                  }
+                >
+                  <div className="d-flex font-weight-bold font">
+                    <div
+                      className="mr-3"
+                      style={{ fontSize: '1.20rem', minWidth: '30px' }}
+                    >
+                      {index + 1}.
+                    </div>
+                    <div className="cap-first definition-text">
+                      {def.definition.toString()}.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ),
+        )}
+      </div>
+      {
+        // only show button if results list is longer than min
+        defs.length > min ? (
+          <div className="d-flex justify-content-center">
+            <button
+              type="button"
+              className="btn btn-secondary m-4"
+              onClick={() => {
+                defsVisible === min ? setDefsVisible(max) : setDefsVisible(min);
+              }}
+            >
+              {`${defsVisible === min ? 'Show All' : 'Show Less'}`}
+            </button>
           </div>
-        </div>
-      ))}
+        ) : (
+          <></>
+        )
+      }
     </div>
   );
 }
--- src/views/WordsPage.jsx
+++ src/views/WordsPage.jsx
@@ -1,24 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import WordsAPI from '../api/WordsAPI';
 import SearchBar from '../components/SearchBar';
 import Definitions from '../components/definitions';
+import { Bar } from 'react-chartjs-2';
+import 'chart.js/auto';
+
+const backgroundColors = [
+  'rgba(54, 162, 235, 0.8)',
+  'rgba(255, 206, 86, 0.8)',
+  'rgba(255, 99, 132, 0.8)',
+  'rgba(75, 192, 192, 0.8)',
+  'rgba(153, 102, 255, 0.8)',
+  'rgba(255, 159, 64, 0.8)',
+  'rgba(199, 199, 199, 0.8)',
+  'rgba(83, 102, 255, 0.8)',
+  'rgba(40, 159, 64, 0.8)',
+  'rgba(210, 199, 199, 0.8)',
+  'rgba(78, 52, 199, 0.8)',
+];
+
+const borderColors = [
+  'rgba(54, 162, 235, 1)',
+  'rgba(255, 206, 86, 1)',
+  'rgba(255, 99, 132, 1)',
+  'rgba(75, 192, 192, 1)',
+  'rgba(153, 102, 255, 1)',
+  'rgba(255, 159, 64, 1)',
+  'rgba(159, 159, 159, 1)',
+  'rgba(83, 102, 255, 1)',
+  'rgba(40, 159, 64, 1)',
+  'rgba(210, 199, 199, 1)',
+  'rgba(78, 52, 199, 1)',
+];
 
 const WordsPage = () => {
+  const options = {};
   const [word, setWord] = useState('');
   const [data, setData] = useState(null);
   const [searchedWord, setSearchedWord] = useState('');
   const [searchAttempt, setSearchAttempt] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
+  const [wordArray, setWordArray] = useState([]);
+  const [freqArray, setFreqArray] = useState([]);
+  const [barData, setBarData] = useState({});
+
+  useEffect(() => {
+    const freqs = freqArray;
+    const wordNames = wordArray;
+    setBarData({
+      labels: wordArray,
+      datasets: [
+        {
+          data: freqs,
+          backgroundColors: backgroundColors,
+          borderColors: borderColors,
+          borderWidth: 2,
+        },
+      ],
+    });
+  }, [wordArray, freqArray]);
+
   const fetchData = async () => {
     setIsLoading(true);
     const data = await WordsAPI(word);
+
+    // console.log('data', data);
+    // console.log('data.word', data.word);
+    // console.log('data.frequency', data.frequency);
+    // console.log('data.results', data.results);
+
     setIsLoading(false);
     setSearchAttempt(true);
     setSearchedWord(word);
     setData(data);
+    if (data.results && data.frequency) {
+      setWordArray((current) => [...current, data.word]);
+      setFreqArray((current) => [...current, data.frequency]);
+    }
     setWord('');
   };
+
+  console.log('wordArray', wordArray);
+  console.log('freqArray', freqArray);
 
   return (
     <section
@@ -34,8 +98,11 @@ const WordsPage = () => {
         onSearch={fetchData}
       />
       {isLoading && <h2>Loading...</h2>}
-      {!isLoading && data && data.definitions && data.definitions.length > 0 ? (
-        <Definitions data={data} />
+      {!isLoading && data && data.results && data.results.length > 0 ? (
+        <>
+          <Definitions data={data} />
+          <Bar data={barData} options={options}></Bar>
+        </>
       ) : (
         !isLoading &&
         searchAttempt && (
