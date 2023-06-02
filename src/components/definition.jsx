29763a22c3ccaff93b2b6828a7054d75bd1686b9
miles <miles4599@gmail.com>
2023-06-02 02:08:37 -0700

    moved the code for a single definition into its own component

---
+++ src/components/definition.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+export default function Definition({ def, index }) {
+  const [hovering, setHovering] = useState(-1);
+  return (
+    <div key={index} className="rounded">
+      {def.isTitle && (
+        <div
+          className="d-flex rounded "
+          style={{ backgroundColor: 'var(--bs-dark)' }}
+        >
+          <h4 className="mt-2 pl-3">
+            <i>{def.partOfSpeech}</i>
+          </h4>
+        </div>
+      )}
+      <div
+        onMouseEnter={() => setHovering(index)}
+        onMouseLeave={() => setHovering(-1)}
+        className="card-body p-3 rounded border border-dark"
+        style={
+          hovering === index
+            ? { backgroundColor: 'var(--bs-dark)' }
+            : { backgroundColor: 'var(--bs-darker)' }
+        }
+      >
+        <div className="d-flex font-weight-bold font">
+          <div
+            className="mr-3"
+            style={{ fontSize: '1.20rem', minWidth: '30px' }}
+          >
+            {index + 1}.
+          </div>
+          <div className="cap-first definition-text">
+            {def.definition.toString()}.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
