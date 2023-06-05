bc7fe320345e3b84075f21bb9e06fb02345fc03c
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2023-06-04 19:30:52 -0700

    done the top ES translation word graph

--- src/components/translate/VisualizeGraphs.jsx
+++ src/components/translate/VisualizeGraphs.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
-import DoughnutChart from "./DonutGraphEN";
+import DoughnutChartEN from "./DonutGraphEN";
+import DoughnutChartES from "./DonutGraphES";
 
 const VisualizeGraphs = ({ getSortedWords }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpenTwo, setIsOpenTwo] = useState(false);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
+    setIsOpenTwo(false); // close the ES graph
+  };
+
+  const toggleDropdownTwo = () => {
+    setIsOpenTwo(!isOpenTwo);
+    setIsOpen(false); // Close the EN graph
   };
 
   return (
@@ -16,43 +24,78 @@ const VisualizeGraphs = ({ getSortedWords }) => {
       data-aos-duration="500"
     >
       <div>
-        <div className="custom-tab card-body d-flex align-items-center justify-content-between border-bottom pb-3 rounded-3">
+        <div className="custom-tab card-body d-flex border-bottom pb-3 rounded-3">
           <h3 className="card-title text-light">Visualization - Graphs</h3>
+          <div className="ml-auto p-2"> </div>
+          {/* close/open tab */}
           {isOpen ? (
             <i
-              className="fa fa-chevron-circle-down "
+              className="fa fa-chevron-circle-down mr-3"
               onClick={toggleDropdown}
               style={{
                 fontSize: 36,
                 cursor: "pointer",
                 color: "black",
               }}
-            ></i>
+            >EN</i>
           ) : (
             <i
-              className="fa fa-chevron-circle-right"
+              className="fa fa-chevron-circle-right mr-3"
               onClick={toggleDropdown}
               style={{
                 fontSize: 36,
                 cursor: "pointer",
               }}
-            ></i>
+            >EN</i>
+          )}
+          {/* close/open tab */}
+          {isOpenTwo ? (
+            <i
+              className="fa fa-chevron-circle-down"
+              onClick={toggleDropdownTwo}
+              style={{
+                fontSize: 36,
+                cursor: "pointer",
+                color: "black",
+              }}
+            >ES</i>
+          ) : (
+            <i
+              className="fa fa-chevron-circle-right"
+              onClick={toggleDropdownTwo}
+              style={{
+                fontSize: 36,
+                cursor: "pointer",
+              }}
+            >ES</i>
           )}
         </div>
 
-        {isOpen && (
+        {/* this handles the graph of the top EN translation words */}
+        {isOpen && !isOpenTwo && (
           <div className="py-4 text-center">
-         
             {getSortedWords.some((word) => word.word.includes("-en")) ? (
               <>
-              <h4 className="pt-2 pb-2">Top 5 English Translation Words</h4>
-              <DoughnutChart getSortedWords={getSortedWords}/>
+                <h4 className="pt-2 pb-2">Top 5 English Translation Words</h4>
+                <DoughnutChartEN getSortedWords={getSortedWords} />
               </>
-              
-            ): (
-              <DoughnutChart getSortedWords={getSortedWords} />
+            ) : (
+              <DoughnutChartEN getSortedWords={getSortedWords} />
             )}
-       
+          </div>
+        )}
+
+        {/* this handles the graph of the top ES translation words */}
+        {!isOpen && isOpenTwo && (
+          <div className="py-4 text-center">
+            {getSortedWords.some((word) => word.word.includes("-en")) ? (
+              <>
+                <h4 className="pt-2 pb-2">Top 5 Spanish Translation Words</h4>
+                <DoughnutChartES getSortedWords={getSortedWords} />
+              </>
+            ) : (
+              <DoughnutChartES getSortedWords={getSortedWords} />
+            )}
           </div>
         )}
       </div>
